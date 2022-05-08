06f2da1db600c16bcfaec73004bf11f5cc51832a
Luke100000 <jonas.peche@aon.at>
2022-05-08 19:51:48 +0200

    fixed #26

---
+++ common/src/main/java/immersive_armors/mixin/MixinPlayerEntity.java
@@ -0,0 +1,53 @@
+package immersive_armors.mixin;
+
+import immersive_armors.Config;
+import immersive_armors.item.ExtendedArmorItem;
+import net.minecraft.client.render.entity.PlayerModelPart;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class MixinPlayerEntity {
+    @Shadow
+    public abstract ItemStack getEquippedStack(EquipmentSlot slot);
+
+    @Inject(method = "isPartVisible", at = @At("HEAD"), cancellable = true)
+    public void isPartVisible(PlayerModelPart modelPart, CallbackInfoReturnable<Boolean> cir) {
+        if (!Config.getInstance().hideSecondLayerUnderArmor) {
+            return;
+        }
+
+        int flag = modelPart.getId();
+        EquipmentSlot slot = null;
+        int index = -1;
+        switch (flag) {
+            case 1, 2, 3 -> {
+                slot = EquipmentSlot.CHEST;
+                index = 1;
+            }
+            case 4, 5 -> {
+                slot = EquipmentSlot.LEGS;
+                index = 2;
+            }
+            case 6 -> {
+                slot = EquipmentSlot.HEAD;
+                index = 0;
+            }
+        }
+
+        if (index >= 0) {
+            ItemStack stack = getEquippedStack(slot);
+            if (stack.getItem() instanceof ExtendedArmorItem armorItem) {
+                if (armorItem.getMaterial().shouldHideSecondLayer()[index]) {
+                    cir.setReturnValue(false);
+                }
+            }
+        }
+    }
+}
