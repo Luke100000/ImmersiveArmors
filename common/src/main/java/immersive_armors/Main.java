8d4d29dd374ef91911ed564e381415efe828d339
Luke100000 <jonas.peche@aon.at>
2022-05-08 19:05:38 +0200

    fixed forge moment

--- common/src/main/java/immersive_armors/Main.java
+++ common/src/main/java/immersive_armors/Main.java
@@ -6,4 +6,6 @@ import org.apache.logging.log4j.Logger;
 public final class Main {
     public static final String MOD_ID = "immersive_armors";
     public static final Logger LOGGER = LogManager.getLogger();
+
+    public static boolean FORGE = false;
 }
