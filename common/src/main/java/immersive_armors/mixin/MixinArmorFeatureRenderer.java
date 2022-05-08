a9f31e870478573376abb0a5ec8540b43eca10f0
Luke100000 <jonas.peche@aon.at>
2022-05-08 17:31:09 +0200

    fixed incompatibilities with some cosmetic mods (#28 and #29)

--- common/src/main/java/immersive_armors/mixin/MixinArmorFeatureRenderer.java
+++ common/src/main/java/immersive_armors/mixin/MixinArmorFeatureRenderer.java
@@ -9,47 +9,49 @@ import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(ArmorFeatureRenderer.class)
 public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
-
     public MixinArmorFeatureRenderer(FeatureRendererContext<T, M> context) {
         super(context);
     }
 
-    @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true)
-    void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci) {
-        ItemStack itemStack = entity.getEquippedStack(armorSlot);
-        if (itemStack.getItem() instanceof ExtendedArmorItem) {
-            ci.cancel();
-        }
+    float tickDelta;
+    ItemStack equippedStack;
+    T entity;
+
+    @Inject(method = "renderArmor", at = @At("HEAD"))
+    private void fetchEntity(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot slot, int light, A model, CallbackInfo ci) {
+        this.entity = entity;
     }
 
-    private void renderDecoModels(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, float tickDelta) {
-        ItemStack itemStack = entity.getEquippedStack(armorSlot);
-        if (itemStack.getItem() instanceof ExtendedArmorItem) {
-            ExtendedArmorItem armorItem = (ExtendedArmorItem)itemStack.getItem();
-            if (armorItem.getSlotType() == armorSlot) {
-                armorItem.getMaterial().getPieces(armorSlot).forEach(piece -> {
-                    //noinspection unchecked
-                    piece.render(matrices, vertexConsumers, light, entity, itemStack, tickDelta, armorSlot, (BipedEntityModel<LivingEntity>)getContextModel());
-                });
-            }
+    @ModifyVariable(method = "renderArmor", at = @At("STORE"), ordinal = 0)
+    private ItemStack fetchItemStack(ItemStack itemStack) {
+        this.equippedStack = itemStack;
+        return itemStack;
+    }
+
+    @Inject(method = "renderArmorParts", at = @At("HEAD"), cancellable = true)
+    void renderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item, boolean usesSecondLayer, A model, boolean legs, float red, float green, float blue, String overlay, CallbackInfo ci) {
+        if (equippedStack.getItem() == item && item instanceof ExtendedArmorItem armorItem) {
+            armorItem.getMaterial().getPieces(item.getSlotType()).forEach(piece -> {
+                //noinspection unchecked
+                piece.render(matrices, vertexConsumers, light, entity, equippedStack, tickDelta, item.getSlotType(), (BipedEntityModel<LivingEntity>)getContextModel());
+            });
+            ci.cancel();
         }
     }
 
     @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At("HEAD"))
     public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T entity, float f, float g, float tickDelta, float j, float k, float l, CallbackInfo ci) {
-        // features
-        renderDecoModels(matrixStack, vertexConsumerProvider, entity, EquipmentSlot.HEAD, i, tickDelta);
-        renderDecoModels(matrixStack, vertexConsumerProvider, entity, EquipmentSlot.CHEST, i, tickDelta);
-        renderDecoModels(matrixStack, vertexConsumerProvider, entity, EquipmentSlot.LEGS, i, tickDelta);
-        renderDecoModels(matrixStack, vertexConsumerProvider, entity, EquipmentSlot.FEET, i, tickDelta);
+        this.tickDelta = tickDelta;
     }
 }
