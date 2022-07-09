d9844e408f6181d00753c025329a253229e09d66
Pug <batterypug@gmail.com>
2022-07-09 21:27:21 +1000

    change getLevel mixin to match 1.18.2 version

--- common/src/main/java/immersive_armors/mixin/MixinEnchantmentHelper.java
+++ common/src/main/java/immersive_armors/mixin/MixinEnchantmentHelper.java
@@ -4,13 +4,10 @@ import immersive_armors.item.ExtendedArmorItem;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class MixinEnchantmentHelper {
@@ -25,29 +22,8 @@ public abstract class MixinEnchantmentHelper {
         return 0;
     }
 
-    private static int getLevelVanilla(Enchantment enchantment, ItemStack stack) {
-        if (!stack.isEmpty()) {
-            Identifier identifier = Registry.ENCHANTMENT.getId(enchantment);
-            NbtList nbtList = stack.getEnchantments();
-
-            for (int i = 0; i < nbtList.size(); ++i) {
-                NbtCompound nbtCompound = nbtList.getCompound(i);
-                Identifier identifier2 = Identifier.tryParse(nbtCompound.getString("id"));
-                if (identifier2 != null && identifier2.equals(identifier)) {
-                    return MathHelper.clamp(nbtCompound.getInt("lvl"), 0, 255);
-                }
-            }
-
-        }
-        return 0;
-    }
-
-    /**
-     * @author Luke100000
-     * @reason Overwrite required since getLevel is public static
-     */
-    @Overwrite
-    public static int getLevel(Enchantment enchantment, ItemStack stack) {
-        return Math.max(getLevelVanilla(enchantment, stack), getStaticLevel(enchantment, stack));
+    @Inject(method = "getLevel", at = @At("RETURN"), cancellable = true)
+    private static void getLevel(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(Math.max(cir.getReturnValue(), getStaticLevel(enchantment, stack)));
     }
 }
