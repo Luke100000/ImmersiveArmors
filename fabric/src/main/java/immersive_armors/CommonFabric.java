da2c1c8ccb6ad240739d52fdbc27a14485a7553c
Luke100000 <jonas.peche@aon.at>
2022-07-20 17:18:43 +0200

    cleanup

--- fabric/src/main/java/immersive_armors/CommonFabric.java
+++ fabric/src/main/java/immersive_armors/CommonFabric.java
@@ -5,13 +5,6 @@ import immersive_armors.cobalt.registration.RegistrationImpl;
 import immersive_armors.server.Command;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.EventFactory;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.util.ActionResult;
 
 public final class CommonFabric implements ModInitializer {
     @Override
