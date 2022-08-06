0f070409669a659b7cb997823a25b6080c0e0671
Luke100000 <jonas.peche@aon.at>
2022-08-06 11:09:16 +0200

    fixed #47

--- common/src/main/java/immersive_armors/util/FlowingText.java
+++ common/src/main/java/immersive_armors/util/FlowingText.java
@@ -11,13 +11,18 @@ import java.util.stream.Collectors;
 
 public class FlowingText {
     public static List<Text> wrap(Text text, int maxWidth) {
-        return MinecraftClient.getInstance().textRenderer.getTextHandler().wrapLines(text, maxWidth, Style.EMPTY).stream().map(line -> {
-            LiteralText compiled = new LiteralText("");
-            line.visit((s, t) -> {
-                compiled.append(new LiteralText(t).setStyle(s));
-                return Optional.empty();
-            }, text.getStyle());
-            return compiled;
-        }).collect(Collectors.toList());
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.world != null && client.isOnThread()) {
+            return client.textRenderer.getTextHandler().wrapLines(text, maxWidth, Style.EMPTY).stream().map(line -> {
+                LiteralText compiled = new LiteralText("");
+                line.visit((s, t) -> {
+                    compiled.append(new LiteralText(t).setStyle(s));
+                    return Optional.empty();
+                }, text.getStyle());
+                return compiled;
+            }).collect(Collectors.toList());
+        } else {
+            return List.of(text);
+        }
     }
 }
