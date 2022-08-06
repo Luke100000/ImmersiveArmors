0575f3887d13c4e604b6af5892b0c98556c755fc
Luke100000 <jonas.peche@aon.at>
2022-08-06 11:16:42 +0200

    cleanup

--- common/src/main/java/immersive_armors/util/FlowingText.java
+++ common/src/main/java/immersive_armors/util/FlowingText.java
@@ -1,61 +1,16 @@
 package immersive_armors.util;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.OrderedText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public final class FlowingText {
-
-    private final List<OrderedText> lines;
-
-    private final float scale;
-
-    public FlowingText(List<OrderedText> lines, float scale) {
-        this.lines = lines;
-        this.scale = scale;
-    }
-
-    public List<OrderedText> lines() {
-        return lines;
-    }
-
-    public float scale() {
-        return scale;
-    }
-
-    public interface Factory {
-        /**
-         * Scales the given text to fit a desired width and height.
-         */
-        static FlowingText wrapLines(TextRenderer renderer, Text text, int maxBlockWidth, int maxBlockHeight) {
-            float scale = 1;
-
-            List<OrderedText> output;
-
-            do {
-                output = renderer.wrapLines(text, (int)Math.ceil(maxBlockWidth / scale));
-
-                if (output.size() * 10 * scale <= maxBlockHeight) {
-                    break;
-                }
-
-                scale -= 0.01F;
-            } while (scale > 0.08F);
-
-            // We trim excess lines in the event fitting isn't perfect
-            int maxLines = (int)Math.ceil(maxBlockHeight / (10 * scale));
-
-            return new FlowingText(output.stream().limit(maxLines).collect(Collectors.toList()), scale);
-        }
-    }
-
     public static List<Text> wrap(Text text, int maxWidth) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.world != null && client.isOnThread()) {
@@ -68,7 +23,7 @@ public final class FlowingText {
                 return compiled;
             }).collect(Collectors.toList());
         } else {
-            return List.of(text);
+            return Collections.singletonList(text);
         }
     }
 }
