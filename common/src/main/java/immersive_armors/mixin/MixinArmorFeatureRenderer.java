b8fc3cee5fb841d80ea3d04926bdc0491d05ff25
Luke100000 <jonas.peche@aon.at>
2022-12-27 22:48:10 +0100

    fixed #65

--- common/src/main/java/immersive_armors/mixin/MixinArmorFeatureRenderer.java
+++ common/src/main/java/immersive_armors/mixin/MixinArmorFeatureRenderer.java
@@ -14,13 +14,16 @@ import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nullable;
 
-@Mixin(ArmorFeatureRenderer.class)
+
+@Mixin(value = ArmorFeatureRenderer.class, priority = 700)
 public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
     @Shadow
     protected abstract A getArmor(EquipmentSlot slot);
@@ -29,26 +32,33 @@ public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extend
         super(context);
     }
 
-    float tickDelta;
-    ItemStack equippedStack;
-    T entity;
+    @Unique
+    float immersiveArmors$tickDelta;
+
+    @Unique
+    @Nullable
+    ItemStack immersiveArmors$equippedStack;
+
+    @Unique
+    @Nullable
+    T immersiveArmors$entity;
 
     @ModifyVariable(method = "renderArmor", at = @At("STORE"), ordinal = 0)
-    private ItemStack fetchItemStack(ItemStack itemStack) {
-        this.equippedStack = itemStack;
+    ItemStack immersiveArmors$fetchItemStack(ItemStack itemStack) {
+        this.immersiveArmors$equippedStack = itemStack;
         return itemStack;
     }
 
     @Inject(method = "renderArmorParts", at = @At("HEAD"), cancellable = true)
-    void injectRenderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item, boolean usesSecondLayer, A model, boolean legs, float red, float green, float blue, String overlay, CallbackInfo ci) {
-        if (!Main.FORGE && equippedStack.getItem() == item && item instanceof ExtendedArmorItem armorItem) {
+    void immersiveArmors$injectRenderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item, boolean usesSecondLayer, A model, boolean legs, float red, float green, float blue, String overlay, CallbackInfo ci) {
+        if (!Main.FORGE && immersiveArmors$equippedStack != null && immersiveArmors$equippedStack.getItem() == item && item instanceof ExtendedArmorItem armorItem) {
             renderPieces(matrices, vertexConsumers, light, armorItem);
             ci.cancel();
         }
     }
 
     @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true)
-    void injectRenderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci) {
+    void immersiveArmors$injectRenderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci) {
         if (Main.FORGE) {
             ItemStack itemStack = entity.getEquippedStack(armorSlot);
             if (itemStack.getItem() instanceof ExtendedArmorItem) {
@@ -58,9 +68,9 @@ public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extend
     }
 
     @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At("HEAD"))
-    public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T entity, float f, float g, float tickDelta, float j, float k, float l, CallbackInfo ci) {
-        this.tickDelta = tickDelta;
-        this.entity = entity;
+    public void immersiveArmors$render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T entity, float f, float g, float tickDelta, float j, float k, float l, CallbackInfo ci) {
+        this.immersiveArmors$tickDelta = tickDelta;
+        this.immersiveArmors$entity = entity;
 
         // Forge removed renderArmorParts calls
         if (Main.FORGE) {
@@ -72,17 +82,21 @@ public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extend
     }
 
     private void renderPieces(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, EquipmentSlot armorSlot) {
-        equippedStack = entity.getEquippedStack(armorSlot);
-        if (equippedStack.getItem() instanceof ExtendedArmorItem armorItem) {
-            renderPieces(matrices, vertexConsumers, light, armorItem);
+        if (immersiveArmors$entity != null) {
+            immersiveArmors$equippedStack = immersiveArmors$entity.getEquippedStack(armorSlot);
+            if (immersiveArmors$equippedStack.getItem() instanceof ExtendedArmorItem armorItem) {
+                renderPieces(matrices, vertexConsumers, light, armorItem);
+            }
         }
     }
 
     private void renderPieces(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ExtendedArmorItem item) {
-        item.getMaterial().getPieces(item.getSlotType()).forEach(piece -> {
-            A armorModel = getArmor(item.getSlotType());
-            this.getContextModel().setAttributes(armorModel);
-            piece.render(matrices, vertexConsumers, light, entity, equippedStack, tickDelta, item.getSlotType(), armorModel);
-        });
+        if (immersiveArmors$entity != null && immersiveArmors$equippedStack != null) {
+            item.getMaterial().getPieces(item.getSlotType()).forEach(piece -> {
+                A armorModel = getArmor(item.getSlotType());
+                this.getContextModel().setAttributes(armorModel);
+                piece.render(matrices, vertexConsumers, light, immersiveArmors$entity, immersiveArmors$equippedStack, immersiveArmors$tickDelta, item.getSlotType(), armorModel);
+            });
+        }
     }
 }
