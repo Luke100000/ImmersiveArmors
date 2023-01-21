4a0542d2d468170573e64138ae74dfc5f7090f32
Luke100000 <jonas.peche@aon.at>
2023-01-21 18:44:32 +0100

    added MixinExtra

--- common/src/main/java/immersive_armors/mixin/MixinEnchantmentHelper.java
+++ common/src/main/java/immersive_armors/mixin/MixinEnchantmentHelper.java
@@ -1,5 +1,6 @@
 package immersive_armors.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import immersive_armors.item.ExtendedArmorItem;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -9,23 +10,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = EnchantmentHelper.class, priority = 1100)
+@Mixin(value = EnchantmentHelper.class, priority = 500)
 public abstract class MixinEnchantmentHelper {
     private static int getEnchantmentLevel(Enchantment enchantment, ItemStack stack) {
+        int level = 0;
         if (stack.getItem() instanceof ExtendedArmorItem item) {
-
             if (item.getMaterial().hasEnchantment(enchantment)) {
-                return item.getMaterial().getEnchantment(enchantment);
+                int e = item.getMaterial().getEnchantment(enchantment);
+                level = Math.min(level + e, enchantment.getMaxLevel());
             }
         }
-        return 0;
+        return level;
     }
 
-    @Inject(method = "getLevel", at = @At("RETURN"), cancellable = true)
+    private static int level;
+
+    @Inject(method = "getLevel", at = @At("HEAD"))
     private static void immersiveArmors$getLevel(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        int level = getEnchantmentLevel(enchantment, stack);
-        if (cir.getReturnValue() < level) {
-            cir.setReturnValue(level);
-        }
+        level = getEnchantmentLevel(enchantment, stack);
+    }
+
+    @SuppressWarnings("unused")
+    @ModifyReturnValue(method = "getLevel", at = @At("RETURN"))
+    private static int immersiveArmors$modifyGetLevel(int oldLevel) {
+        return Math.max(oldLevel, level);
     }
 }
