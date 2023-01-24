d14ae834ed6cba4ab84fa9768751a9fa23e82051
Luke100000 <jonas.peche@aon.at>
2023-01-24 11:37:28 +0100

    temporary removed extramixins

--- common/src/main/java/immersive_armors/mixin/MixinEnchantmentHelper.java
+++ common/src/main/java/immersive_armors/mixin/MixinEnchantmentHelper.java
@@ -1,6 +1,5 @@
 package immersive_armors.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import immersive_armors.item.ExtendedArmorItem;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -10,27 +9,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = EnchantmentHelper.class)
+@Mixin(value = EnchantmentHelper.class, priority = 1100)
 public abstract class MixinEnchantmentHelper {
     private static int getEnchantmentLevel(Enchantment enchantment, ItemStack stack) {
-        if (stack.getItem() instanceof ExtendedArmorItem item) {
-            if (item.getMaterial().hasEnchantment(enchantment)) {
-                return item.getMaterial().getEnchantment(enchantment);
-            }
+        if (stack.getItem() instanceof ExtendedArmorItem item && item.getMaterial().hasEnchantment(enchantment)) {
+            return item.getMaterial().getEnchantment(enchantment);
         }
         return 0;
     }
 
-    private static int level;
-
-    @Inject(method = "getLevel", at = @At("HEAD"))
+    @Inject(method = "getLevel", at = @At("RETURN"), cancellable = true)
     private static void immersiveArmors$getLevel(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        level = getEnchantmentLevel(enchantment, stack);
-    }
-
-    @SuppressWarnings("unused")
-    @ModifyReturnValue(method = "getLevel", at = @At("RETURN"))
-    private static int immersiveArmors$modifyGetLevel(int oldLevel) {
-        return Math.max(oldLevel, level);
+        int level = getEnchantmentLevel(enchantment, stack);
+        if (cir.getReturnValue() < level) {
+            cir.setReturnValue(level);
+        }
     }
 }
